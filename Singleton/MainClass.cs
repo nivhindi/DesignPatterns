f213e283f775a952d77ac782cdfe610a9b2c4aa5
nivhindi <52877509+nivhindi@users.noreply.github.com>
2019-07-14 11:52:52 +0300

    Add files via upload

---
+++ Singleton/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            Singleton s3 = Singleton.GetInstance();
+            s1.printCurrentTime();
+            s2.printCurrentTime();
+            s3.printCurrentTime();
+        }
+    }
+}
